5ceff14ab2f883d070cbee0ea2a613273f63e68f
iosdeveloper <max.iphone.dev@gmail.com>
2016-04-15 10:33:58 +0200

    Update

--- screenshot.sh
+++ screenshot.sh
@@ -3,11 +3,11 @@
 screenshot() {
   open BitBar.app
 
-  #chmod +x Plugins/cycle_text_and_detail.sh
+  chmod +x Plugins/brew-updates.1h.sh
 
   img_file="$PWD/screenshot.png"
 
-  open "bitbar://screenshot?pluginPath=$PWD/Plugins/cycle_text_and_detail.sh&dst=${img_file}&margin=10"
+  open "bitbar://screenshot?pluginPath=$PWD/Plugins/brew-updates.1h.sh&dst=${img_file}&margin=10"
 
   while [ ! -f "${img_file}" ]
   do
