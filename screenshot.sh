0eda2921ede00eed3e418cb13ae0f5ce308f7a43
iosdeveloper <max.iphone.dev@gmail.com>
2016-04-15 15:52:56 +0200

    Update

--- screenshot.sh
+++ screenshot.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 screenshot() {
-  open BitBar.app
-
   img_file="$PWD/screenshot.png"
 
-  open "bitbar://screenshot?pluginPath=$PWD/Plugins/brew-updates.1h.sh&dst=${img_file}&margin=10"
+  open "bitbar://screenshot?pluginPath=$PWD/Plugins/$1&dst=${img_file}&margin=10"
 
   while [ ! -f "${img_file}" ]
   do
     sleep 1
   done
-
-  killall BitBar
 
   response="$(curl -L -H "Authorization: Client-ID $IMGUR_API_KEY" -F "image=@\"${img_file}\"" https://api.imgur.com/3/image)"
 
@@ -31,13 +27,21 @@ screenshot() {
 defaults write com.apple.dock autohide -bool true; killall Dock
 defaults write com.apple.finder CreateDesktop -bool false; killall Finder
 
-curl -L https://github.com/matryer/bitbar/releases/download/v2.0.0-beta2/BitBar-v2.0.0-beta2.zip > BitBar.zip
+curl -L https://github.com/matryer/bitbar/releases/download/v2.0.0-beta3/BitBar-v2.0.0-beta3.zip > BitBar.zip
 unzip BitBar.zip
 
 defaults write com.matryer.BitBar pluginsDirectory "$PWD/Plugins"
 
 chmod +x Plugins/brew-updates.1h.sh
 
-screenshot
+open BitBar.app
+
+screenshot cycle_text_and_detail.sh
+screenshot brew-updates.1h.sh
+
+killall BitBar
 dark-mode --mode Dark
-screenshot
+open BitBar.app
+
+screenshot cycle_text_and_detail.sh
+screenshot brew-updates.1h.sh
